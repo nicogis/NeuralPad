9679cd041ac526cc8c6c66f3bd8cb616479f992d
Domenico Ciavarella <dciavarella.studioat@gmail.com>
2026-09-20 10:23:52 +0200

    Add NeuralPad initial WPF PoC

---
+++ NeuralPad.Core/NeuralNetwork.cs
@@ -0,0 +1,111 @@
+namespace NeuralPad.Core;
+
+public sealed class NeuralNetwork
+{
+    public List<Layer> Layers { get; } = [];
+    public List<Connection> Connections { get; } = [];
+
+    public IReadOnlyList<ForwardStep> LastTrace { get; private set; } = [];
+
+    public Layer AddLayer(int neuronCount, string name, ActivationKind activation)
+    {
+        if (neuronCount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(neuronCount));
+
+        var layer = new Layer(Layers.Count, name, activation);
+        for (var i = 0; i < neuronCount; i++)
+            layer.Neurons.Add(new Neuron($"{name}{i + 1}", layer.Index, i, activation));
+
+        Layers.Add(layer);
+        return layer;
+    }
+
+    public void FullyConnect(Layer from, Layer to, Func<int, int, double> weightFactory)
+    {
+        foreach (var source in from.Neurons)
+        foreach (var target in to.Neurons)
+            Connections.Add(new Connection(source, target, weightFactory(source.Index, target.Index)));
+    }
+
+    public IReadOnlyList<ForwardStep> Forward(params double[] inputs)
+    {
+        if (Layers.Count < 2)
+            throw new InvalidOperationException("The network must contain at least two layers.");
+        if (inputs.Length != Layers[0].Neurons.Count)
+            throw new ArgumentException("Input count does not match the input layer.", nameof(inputs));
+
+        var trace = new List<ForwardStep>();
+        var sequence = 0;
+
+        for (var i = 0; i < inputs.Length; i++)
+        {
+            var neuron = Layers[0].Neurons[i];
+            neuron.Z = inputs[i];
+            neuron.Activation = inputs[i];
+            trace.Add(new(++sequence, ForwardStepKind.Input, neuron.Name,
+                $"{neuron.Name} = {inputs[i]:0.####}", inputs[i], neuron));
+        }
+
+        for (var layerIndex = 1; layerIndex < Layers.Count; layerIndex++)
+        {
+            var layer = Layers[layerIndex];
+            foreach (var neuron in layer.Neurons)
+            {
+                var incoming = Connections.Where(c => c.To == neuron).ToArray();
+                var sum = 0.0;
+
+                foreach (var connection in incoming)
+                {
+                    connection.Contribution = connection.From.Activation * connection.Weight;
+                    sum += connection.Contribution;
+                    trace.Add(new(++sequence, ForwardStepKind.Contribution,
+                        connection.ToString(),
+                        $"{connection.From.Name}.a x {connection.Weight:0.####} = {connection.Contribution:0.####}",
+                        connection.Contribution, neuron, connection));
+                }
+
+                trace.Add(new(++sequence, ForwardStepKind.WeightedSum, $"{neuron.Name} weighted sum",
+                    $"sum(x_i*w_i) = {sum:0.####}", sum, neuron));
+
+                neuron.Z = sum + neuron.Bias;
+                trace.Add(new(++sequence, ForwardStepKind.Bias, $"{neuron.Name} bias",
+                    $"{sum:0.####} + {neuron.Bias:0.####} = {neuron.Z:0.####}", neuron.Z, neuron));
+
+                neuron.Activation = ActivationFunctions.Apply(neuron.ActivationKind, neuron.Z);
+                trace.Add(new(++sequence,
+                    layerIndex == Layers.Count - 1 ? ForwardStepKind.Output : ForwardStepKind.Activation,
+                    neuron.Name,
+                    $"{neuron.ActivationKind}({neuron.Z:0.####}) = {neuron.Activation:0.####}",
+                    neuron.Activation, neuron));
+            }
+        }
+
+        LastTrace = trace;
+        return trace;
+    }
+
+    public static NeuralNetwork CreateDemo()
+    {
+        var network = new NeuralNetwork();
+        var input = network.AddLayer(2, "X", ActivationKind.Linear);
+        var hidden = network.AddLayer(3, "H", ActivationKind.ReLU);
+        var output = network.AddLayer(1, "O", ActivationKind.Sigmoid);
+
+        var hiddenWeights = new double[,]
+        {
+            { 0.72, -0.31, 0.84 },
+            { -0.42, 0.61, 0.11 }
+        };
+
+        network.FullyConnect(input, hidden, (i, j) => hiddenWeights[i, j]);
+        hidden.Neurons[0].Bias = 0.10;
+        hidden.Neurons[1].Bias = -0.05;
+        hidden.Neurons[2].Bias = 0.02;
+
+        var outputWeights = new[] { 0.82, -0.31, 0.46 };
+        network.FullyConnect(hidden, output, (i, _) => outputWeights[i]);
+        output.Neurons[0].Bias = 0.15;
+
+        return network;
+    }
+}
