e3001e87030c8c1fb735d454c060365fd2b4ce82
Domenico Ciavarella <dciavarella.studioat@gmail.com>
2026-09-20 10:23:42 +0200

    Add NeuralPad initial WPF PoC

---
+++ NeuralPad.Core/Neuron.cs
@@ -0,0 +1,23 @@
+namespace NeuralPad.Core;
+
+public sealed class Neuron
+{
+    public Neuron(string name, int layerIndex, int index, ActivationKind activation)
+    {
+        Name = name;
+        LayerIndex = layerIndex;
+        Index = index;
+        ActivationKind = activation;
+    }
+
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Name { get; }
+    public int LayerIndex { get; }
+    public int Index { get; }
+    public ActivationKind ActivationKind { get; }
+    public double Bias { get; set; }
+    public double Z { get; internal set; }
+    public double Activation { get; internal set; }
+
+    public override string ToString() => Name;
+}
