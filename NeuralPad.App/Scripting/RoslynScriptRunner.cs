682a79ea71dbb858c1f9a9e0e0a51df214a3e16b
Domenico Ciavarella <dciavarella.studioat@gmail.com>
2026-09-24 16:04:44 +0200

    Execute NeuralPad scripts with Roslyn

---
+++ NeuralPad.App/Scripting/RoslynScriptRunner.cs
@@ -0,0 +1,30 @@
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using NeuralPad.App.ViewModels;
+
+namespace NeuralPad.App.Scripting;
+
+public static class RoslynScriptRunner
+{
+    public static async Task<NeuralScriptResult> RunAsync(string code)
+    {
+        ScriptNetwork? dumped = null;
+        var globals = new NeuralScriptGlobals(network => dumped = network);
+
+        var options = ScriptOptions.Default
+            .AddReferences(typeof(RoslynScriptRunner).Assembly, typeof(NeuralPad.Core.NeuralNetwork).Assembly)
+            .AddImports(
+                "System",
+                "System.Linq",
+                "System.Collections.Generic",
+                "NeuralPad.Core",
+                "NeuralPad.App.Scripting");
+
+        await CSharpScript.RunAsync(code, options, globals, typeof(NeuralScriptGlobals));
+
+        if (dumped is null)
+            throw new InvalidOperationException("The script must call Dump(net) to send a network to NeuralPad.");
+
+        return dumped.Build();
+    }
+}
